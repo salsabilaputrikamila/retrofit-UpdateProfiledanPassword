1ca6b64b303d898e37d8f6e0b5fc8dfbd7fbb0f4
owner <salsabilaputrikamila23@gmail.com>
2020-03-23 11:18:24 +0700

    retrofit-salsabilaputrikamila

--- app/src/main/java/id/putraprima/retrofit/api/services/ApiInterface.java
+++ app/src/main/java/id/putraprima/retrofit/api/services/ApiInterface.java
@@ -3,7 +3,10 @@ package id.putraprima.retrofit.api.services;
 
 import id.putraprima.retrofit.api.models.AppVersion;
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
 
 public interface ApiInterface{
     @GET("/")
